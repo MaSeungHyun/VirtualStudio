be69d9ac7cf9a77edfe7dfa1f2f3d7a559e855ee
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-08-05 16:33:21 +0900

    feat: 텍스트 사이즈 수정 및 Panel로 랩핑

--- src/pages/inspector/index.tsx
+++ src/pages/inspector/index.tsx
@@ -1,15 +1,19 @@
 import Toolbar from "@/components/Toolbar";
 import Icon from "@/components/Icon";
+import Panel from "@/components/Panel";
+import { memo } from "react";
 
-export default function Inspector() {
+export const Inspector = memo(() => {
   return (
-    <main className="bg-black-300 flex h-full max-h-full w-full min-w-full flex-col rounded-t-lg">
-      <Toolbar.Container className="h-[1.8rem] justify-between px-[0.5rem]">
-        <div className="flex items-center gap-1">
-          <Icon icon="Hammer" size={14} />
-          <p>Inspector</p>
-        </div>
-      </Toolbar.Container>
-    </main>
+    <Panel>
+      <section className="bg-black-300 flex h-full max-h-full w-full min-w-full flex-col rounded-t-lg">
+        <Toolbar.Container className="justify-between px-[0.5rem]">
+          <div className="flex items-center gap-1">
+            <Icon icon="Hammer" size={14} />
+            <p className="text-xs">Inspector</p>
+          </div>
+        </Toolbar.Container>
+      </section>
+    </Panel>
   );
-}
+});
