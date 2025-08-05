d007bda58540b48344b497b9c1dc475872bfee44
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-08-05 16:27:58 +0900

    feat: Asset 로고 클릭 시, 패널 여닫이 기능 클릭 이벤트 추가

--- src/pages/asset/index.tsx
+++ src/pages/asset/index.tsx
@@ -1,15 +1,29 @@
 import Toolbar from "@/components/Toolbar";
 import Icon from "@/components/Icon";
+import { useSplitStore } from "../scene-editor";
+import { memo, useCallback } from "react";
+import Panel from "@/components/Panel";
 
-export default function Asset() {
+export const Asset = memo(() => {
+  const { open, setOpen } = useSplitStore();
+
+  const handleClickOpenAsset = useCallback(() => {
+    setOpen(!open);
+  }, [open]);
+
   return (
-    <main className="bg-black-300 flex h-full max-h-full w-full min-w-full flex-col rounded-t-lg">
-      <Toolbar.Container className="justify-between px-[0.5rem]">
-        <div className="flex items-center gap-1">
-          <Icon icon="Folder" size={14} />
-          <p>Asset</p>
-        </div>
-      </Toolbar.Container>
-    </main>
+    <Panel>
+      <section className="bg-black-300 flex h-full max-h-full w-full min-w-full flex-col rounded-t-lg">
+        <Toolbar.Container className="justify-between px-[0.5rem]">
+          <div
+            className="hover:bg-black-50 flex cursor-pointer items-center gap-1 rounded-sm px-1"
+            onClick={handleClickOpenAsset}
+          >
+            <Icon icon="Folder" size={14} />
+            <p>Asset</p>
+          </div>
+        </Toolbar.Container>
+      </section>
+    </Panel>
   );
-}
+});
